dd4a923fa375b82e9910e1ad47d6f5e8c4303510
Yugant <yugant35@gmail.com>
2021-01-03 18:05:24 +0530

    create super user function added in django model

--- project_api/models.py
+++ project_api/models.py
@@ -20,7 +20,15 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, name, password):
+        """Create a super user profile"""
+        user = self.create_user(email,name,password)
 
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
